3277a27b4eafe0cc6809ce6473711e4b01f389ff
papatrix <58409502+papatrix@users.noreply.github.com>
2020-04-19 19:48:35 -0400

    Push scraper results to aws (#1)

--- scraper/qc_properties_for_rent.py
+++ scraper/qc_properties_for_rent.py
@@ -1,19 +1,39 @@
+import argparse
+import datetime
+import os
+import re
+import time
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import pandas as pd
-import time
-import re
-import datetime
+from sqlalchemy import create_engine
 from tqdm import tqdm
 
 
-def get_all_property_urls(browser):
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t",
+        "--task",
+        choices=["rentals", "sales"],
+        required=True,
+        help="Type of property to scrape data for. Must be either `rent` or `sale`.",
+    )
+    return parser.parse_args()
+
+
+SITE_URLS = {
+    "rentals": "https://www.centris.ca/en/properties~for-rent?uc=2&view=Thumbnail",
+    "sales": "https://www.centris.ca/en/multi-family-properties~for-sale?view=Thumbnail",
+}
+
+
+def get_all_property_urls(browser, task):
     property_urls = []
     mls_ids = []
-    browser.get(
-        "https://www.centris.ca/en/properties~for-rent?uc=2&view=Thumbnail"
-    )
+    browser.get(SITE_URLS[task])
     time.sleep(7)
 
     num_pages = browser.find_element_by_xpath('//li[@class="pager-current"]').text
@@ -59,7 +79,7 @@ def find_carac_title_element_text(soup, search_text, num_elements):
     return text
 
 
-def get_property_info(property_url, mls_id):
+def get_rental_property_info(property_url, mls_id):
     # make a GET request to fetch the raw HTML content
     html_content_property = requests.get(property_url).text
 
@@ -72,13 +92,15 @@ def get_property_info(property_url, mls_id):
     property_type = property_type.replace("\xa0", " ")
 
     if "sale" in property_type:
-        raise ValueError("Property is for sale. We are concerned with properties to rent.")
+        raise ValueError(
+            "Property is for sale. We are concerned with properties to rent."
+        )
     else:
         property_type = property_type.replace("for rent", "").strip()
 
-    num_bedrooms = soup.find('div', text=re.compile('bedroom')).text
+    num_bedrooms = soup.find("div", text=re.compile("bedroom")).text
     num_bedrooms = int("".join([c for c in num_bedrooms if c.isdigit()]))
-    num_bathrooms = soup.find('div', text=re.compile('bathroom')).text
+    num_bathrooms = soup.find("div", text=re.compile("bathroom")).text
     num_bathrooms = int("".join([c for c in num_bathrooms if c.isdigit()]))
 
     # Date
@@ -100,13 +122,9 @@ def get_property_info(property_url, mls_id):
         else:
             neighborhood = None
     # Area
-    area = find_carac_title_element_text(
-        soup, "Gross area", 4
-    )
+    area = find_carac_title_element_text(soup, "Gross area", 4)
     if area is None:
-        area = find_carac_title_element_text(
-            soup, "Net area", 4
-        )
+        area = find_carac_title_element_text(soup, "Net area", 4)
     if area is not None:
         area = area.replace(" sqft", "")
         area = area.replace(",", "")
@@ -128,7 +146,7 @@ def get_property_info(property_url, mls_id):
     if description is not None:
         description = description.text.replace("\r\n", "").strip()
 
-    rent = soup.find('span', id='BuyPrice')
+    rent = soup.find("span", id="BuyPrice")
     rent = rent["content"]
     rent = float(rent)
 
@@ -149,31 +167,155 @@ def get_property_info(property_url, mls_id):
         "property_type": property_type,
         "description": description,
         "url": property_url,
-        "mls_id": mls_id
+        "mls_id": mls_id,
     }
 
 
+def get_sale_property_info(property_url, mls_id):
+    # make a GET request to fetch the raw HTML content
+    html_content_property = requests.get(property_url).text
+
+    # parse the html content with BS
+    soup = BeautifulSoup(html_content_property, "lxml")
+
+    # Property Type
+    property_type = soup.find("h1", itemprop="category").text
+    property_type = property_type.replace("\n", "")
+    property_type = property_type.replace("\xa0", " ")
+
+    unit_details = soup.find_all("span", attrs={"data-id": "NbUniteFormatted"})
+    unit_description = [detail.text for detail in unit_details if "(" in detail.text][0]
+    num_units = "".join([c for c in unit_description if c.isdigit()])
+    unit_type = "".join([c for c in unit_description if c.isalpha()])
+
+    main_unit_description = [
+        detail.text for detail in unit_details if "room" in detail.text
+    ]
+    if main_unit_description:
+        _, num_bedrooms, num_bathrooms = main_unit_description[0].split(",")
+        num_bedrooms = int("".join([c for c in num_bedrooms if c.isdigit()]))
+        num_bathrooms = int("".join([c for c in num_bathrooms if c.isdigit()]))
+    else:
+        num_bathrooms = None
+        num_bedrooms = None
+
+    claimed_revenue = find_carac_title_element_text(soup, "Potential gross revenue", 4)
+    if claimed_revenue is not None:
+        claimed_revenue = claimed_revenue.replace("$", "")
+        claimed_revenue = claimed_revenue.replace(",", "")
+        claimed_revenue = float(claimed_revenue)
+
+    latitude = float(soup.find(id="PropertyLat").string)
+
+    # Longitude
+    longitude = float(soup.find(id="PropertyLng").string)
+
+    # Price
+    price = float(soup.find("span", id="BuyPrice")["content"])
+
+    # Date
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    # Full Address
+    full_address = soup.find("h2", itemprop="address")
+    full_address = full_address.text
+    address_parts = full_address.split(",")
+    city = address_parts[2]
+    neighborhood = [part for part in address_parts if "Neighbourhood" in part]
+    if neighborhood:
+        neighborhood = neighborhood[0].replace("Neighbourhood", "").strip()
+    else:
+        if "(" in city:
+            neighborhood = city[city.find("(") + 1 : city.find(")")]
+        else:
+            neighborhood = None
+    # Area
+    building_area = find_carac_title_element_text(
+        soup, "Building area (at ground level)", 4
+    )
+    if building_area is not None:
+        building_area = building_area.replace(" sqft", "")
+        building_area = building_area.replace(",", "")
+        building_area = float(building_area)
+
+    lot_area = find_carac_title_element_text(soup, "Lot area", 4)
+    if lot_area is not None:
+        lot_area = lot_area.replace(" sqft", "")
+        lot_area = lot_area.replace(",", "")
+        lot_area = float(lot_area)
+
+    # Year Built
+    year_built = find_carac_title_element_text(soup, "Year built", 4)
+
+    # Additional Features
+    extra_features = find_carac_title_element_text(soup, "Additional features", 4)
+    parking = find_carac_title_element_text(soup, "Parking", 4)
+    pool = find_carac_title_element_text(soup, "Pool", 3)
+    description = soup.find("div", itemprop="description")
+    if description is not None:
+        description = description.text.replace("\r\n", "").strip()
+
+    # Unique ID (Date+MLS)
+    unique_id = date + "-" + str(mls_id)
+
+    return {
+        "price": price,
+        "latitude": latitude,
+        "longitude": longitude,
+        "num_units": num_units,
+        "unit_type": unit_type,
+        "claimed_revenue": claimed_revenue,
+        "full_address": full_address,
+        "city": city,
+        "neighborhood": neighborhood,
+        "year_built": year_built,
+        "extra_features": extra_features,
+        "num_bathrooms": num_bathrooms,
+        "num_bedrooms": num_bedrooms,
+        "building_area": building_area,
+        "lot_area": lot_area,
+        "parking": parking,
+        "pool": pool,
+        "unique_id": unique_id,
+        "property_type": property_type,
+        "description": description,
+        "url": property_url,
+        "mls_id": mls_id,
+    }
+
+
+def _save_results_to_aws(data, task):
+    uri = os.environ["DB_URI"]
+    engine = create_engine(uri)
+    data.to_sql(name=task, con=engine, if_exists="append", index=False)
+
+
 def main():
+    args = _parse_args()
+    task = args.task
+
     browser = webdriver.Chrome("./chromedriver")
-    property_urls, mls_ids = get_all_property_urls(browser)
+    property_urls, mls_ids = get_all_property_urls(browser, task)
     data = []
-    errors = []
+    if task == "rentals":
+        func = get_rental_property_info
+    else:
+        func = get_sale_property_info
     for url, mls_id in tqdm(list(zip(property_urls, mls_ids))):
         try:
-            info = get_property_info(url, mls_id)
+            info = func(url, mls_id)
         except Exception as e:
             print(url)
             print(e, "\n")
-            errors.append(url)
         else:
             data.append(info)
 
-    print(f"Successfully extracted {len(data)} / {len(data) + len(errors)} properties")
+    print(f"Successfully extracted {len(data)} / {len(property_urls)} properties")
     # Enter values in DF
-    df_sale_plexes = pd.DataFrame(data)
-    df_sale_plexes.to_csv("rent.csv", index=False)
-    with open("error_log.csv", "w") as f:
-        f.write(",\n".join(errors))
+    df = pd.DataFrame(data)
+
+    _save_results_to_aws(df, task)
+    print("Pushed results to AWS")
 
 
 if __name__ == "__main__":
