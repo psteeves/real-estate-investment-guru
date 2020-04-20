586d2389543eafe6e4b71f6ea69278e3ee6f15fc
papatrix <58409502+papatrix@users.noreply.github.com>
2020-04-19 20:02:10 -0400

    Add placeholder dash app and heroku setup (#2)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python dash/app.py
---
+++ dash/app.py
@@ -0,0 +1,11 @@
+import dash
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1('Hello Dash')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
