fb6ba32f246d322121924f75c4025aac0a055676
Pat Steeves <patricksteeves02@gmail.com>
2020-04-22 15:36:20 -0400

    add slider

--- dash/app.py
+++ dash/app.py
@@ -1,3 +1,4 @@
+import dash_core_components as dcc
 import dash_html_components as html
 
 import dash
@@ -6,12 +7,39 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        html.H2("Real Estate Investment Guru"),
-        html.H5("A tool to help guide your investment decisions")
+        html.Div(
+            [
+                html.H1("Real Estate Investment Guru"),
+                html.H4("A tool to help guide your investment decisions"),
+            ],
+            id="app-header",
+        ),
+        html.Div(
+            [
+                html.P("What is your downpayment budget?", className="control-label",),
+                dcc.Slider(
+                    id="downpayment_budget_slider",
+                    min=10,
+                    max=500,
+                    step=10,
+                    marks={
+                        10: "$10K",
+                        100: "$100K",
+                        200: "$200K",
+                        300: "$300K",
+                        400: "$400K",
+                        500: "$500K",
+                    },
+                    tooltip={'visible': True},
+                    className="dcc-control",
+                ),
+            ],
+            className="pretty-container",
+        ),
     ]
 )
 
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, dev_tools_props_check=False)
