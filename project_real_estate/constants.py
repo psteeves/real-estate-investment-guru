47dedd909519e56e44e14c598cc6ec4011fc9161
Pat Steeves <patricksteeves02@gmail.com>
2020-04-28 16:09:05 -0400

    fix serialized_model_path

--- project_real_estate/constants.py
+++ project_real_estate/constants.py
@@ -1,4 +1,6 @@
+from pathlib import Path
+
 MAX_NUM_RESULTS = 10
 COLUMNS_TO_DISPLAY = ["full_address", "city", "price", "predicted_rent_revenue", "url"]
 ALLOWED_PROPERTY_TYPES = ["Condo / Apartment", "Loft / Studio"]
-SERIALIZED_MODEL_PATH = "serialized_models/rent_predictor.pkl"
+SERIALIZED_MODEL_PATH = Path(__file__).parent.parent / "serialized_models/rent_predictor.pkl"
