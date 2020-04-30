9af6f442c263c69861ced56c3856737fb89925cb
Pat Steeves <patricksteeves02@gmail.com>
2020-04-29 21:27:04 -0400

    add financial model

--- project_real_estate/constants.py
+++ project_real_estate/constants.py
@@ -1,6 +1,16 @@
 from pathlib import Path
 
-MAX_NUM_RESULTS = 10
-COLUMNS_TO_DISPLAY = ["full_address", "city", "price", "predicted_rent_revenue", "url"]
+MAX_NUM_RESULTS = 20
+COLUMNS_TO_DISPLAY = [
+    "Address",
+    "City",
+    "Price",
+    "Predicted Rent Revenue",
+    "Cash on Cash Return",
+    "Equity Return on Investment",
+    "URL",
+]
 ALLOWED_PROPERTY_TYPES = ["Condo / Apartment", "Loft / Studio"]
-SERIALIZED_MODEL_PATH = Path(__file__).parent.parent / "serialized_models/rent_predictor.pkl"
+SERIALIZED_MODEL_PATH = (
+    Path(__file__).parent.parent / "serialized_models/rent_predictor.pkl"
+)
--- project_real_estate/dash_app/app.py
+++ project_real_estate/dash_app/app.py
@@ -2,15 +2,18 @@ import dash
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from project_real_estate.constants import MAX_NUM_RESULTS
+from project_real_estate.constants import COLUMNS_TO_DISPLAY, MAX_NUM_RESULTS
 from project_real_estate.dash_app.layout import (
     app_header,
     reports_section,
     results_list,
-    sales_data_display_with_predictions,
+    sales_data_display_with_rent_predictions,
     user_inputs,
 )
-from project_real_estate.models.financial_model import TrivialFinancialModel
+from project_real_estate.models.financial_model import (
+    SimpleFinancialModel,
+    TrivialFinancialModel,
+)
 from project_real_estate.models.rent_estimator import TrivialRentEstimator
 
 app = dash.Dash(__name__)
@@ -23,20 +26,61 @@ rent_model = TrivialRentEstimator()
 
 @app.callback(
     Output(component_id="table", component_property="data"),
-    [Input(component_id="city_options", component_property="value")],
+    [
+        Input(component_id="city", component_property="value"),
+        Input(component_id="budget", component_property="value"),
+        Input(component_id="downpayment", component_property="value"),
+        Input(component_id="closing_fees", component_property="value"),
+        Input(component_id="interest_rate", component_property="value"),
+        Input(component_id="amortization_period", component_property="value"),
+        Input(component_id="vacancy_rate", component_property="value"),
+        Input(component_id="rent_increase", component_property="value"),
+        Input(component_id="expense_ratio", component_property="value"),
+        Input(component_id="yearly_reserves", component_property="value"),
+    ],
 )
-def update_report(city_filters):
+def predict_roi(
+    city_filters,
+    budget,
+    downpayment,
+    closing_fees,
+    interest_rate,
+    amortization,
+    vacancy,
+    rate_rent_increase,
+    expense_ratio,
+    yearly_reserves,
+):
     if not city_filters:
-        return sales_data_display_with_predictions.iloc[:MAX_NUM_RESULTS, :].to_dict(
-            "rows"
-        )
-    return (
-        sales_data_display_with_predictions[
-            sales_data_display_with_predictions.City.isin(city_filters)
+        sales_by_city = sales_data_display_with_rent_predictions
+    else:
+        sales_by_city = sales_data_display_with_rent_predictions[
+            sales_data_display_with_rent_predictions.City.isin(city_filters)
         ]
-        .iloc[:MAX_NUM_RESULTS, :]
-        .to_dict("rows")
+
+    sales_under_budget = sales_by_city[sales_by_city.Price < budget]
+
+    finance_model = SimpleFinancialModel(
+        downpayment=downpayment,
+        closing_fees=closing_fees,
+        interest_rate=interest_rate,
+        amortization=amortization,
+        vacancy=vacancy,
+        rate_rent_increase=rate_rent_increase,
+        expense_ratio=expense_ratio,
+        yearly_reserves=yearly_reserves,
     )
+    prediction = finance_model.predict(sales_under_budget).loc[:, COLUMNS_TO_DISPLAY]
+    prediction = prediction.sort_values(
+        by="Equity Return on Investment", ascending=False
+    )
+    prediction["Equity Return on Investment"] = prediction[
+        "Equity Return on Investment"
+    ].apply(lambda x: f"{x:.1%}")
+    prediction["Cash on Cash Return"] = prediction["Cash on Cash Return"].apply(
+        lambda x: f"{x:.1%}"
+    )
+    return prediction.iloc[:MAX_NUM_RESULTS].to_dict("rows")
 
 
 server = app.server
