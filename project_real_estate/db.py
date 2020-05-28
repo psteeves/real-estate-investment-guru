883105f21990922422ba93e2ee521437e6ed9736
Pat Steeves <patricksteeves02@gmail.com>
2020-05-28 15:39:53 -0400

    limit size of db query when pulling properties

--- project_real_estate/db.py
+++ project_real_estate/db.py
@@ -4,10 +4,10 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def _pull_data(table_name):
+def _pull_data(table_name, max_rows=30000):
     db_uri = os.environ["DB_URI"]
     engine = create_engine(db_uri)
-    sql = f"SELECT * FROM {table_name};"
+    sql = f"SELECT * FROM {table_name} LIMIT {max_rows};"
     data = pd.read_sql(sql=sql, con=engine)
     return data
 
